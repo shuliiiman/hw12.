c543fdc8d8056ecc27375896a204364c3c926a1c
aleksandrshulimov <a.shulimov98@gmail.com>
2024-02-15 07:14:58 +0300

    2 commit.

--- src/Author.java
+++ src/Author.java
@@ -5,10 +5,27 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    public String getFirstName() { //создание геттера
+    public String getFirstName() { //создание геттера.
         return this.firstName;
     }
     public String getLastName() {
         return this.lastName;
+    }
+    @Override
+    public String toString(){//метод toString
+        return
+                this.firstName + " " + this.lastName + ". ";
+    }
+    @Override
+    public boolean equals(Object other) { //метод equals
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author author = (Author) other;//(сравнение объекта класса  с другими объектами этого класса)
+        return firstName.equals(author.firstName);//(сравниваемое свойство)
+    }
+    @Override
+    public int hashCode() {//Метод hashCode
+        return java.util.Objects.hash(firstName);
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,17 @@
 public class Book {
-    private String name;//поле класса
-    private Author firstAndLastName;
+    private String name;//поле класса.
+    private Author author;
     private int yearOfPublication;
-    public Book(String name, Author firstAndLastName, int yearOfPublication) {//метод конструктор
+    public Book(String name, Author author, int yearOfPublication) {//метод конструктор.
         this.name=name;
-        this.firstAndLastName=firstAndLastName;
+        this.author=author;
         this.yearOfPublication=yearOfPublication;
     }
-    public String getName() {//геттер
+    public String getName() {//геттер.
         return this.name;
     }
     public Author getAuthor() {
-        return this.firstAndLastName;
+        return this.author;
     }
     public int getYearOfPublication() {
         return this.yearOfPublication;
@@ -19,5 +19,23 @@ public class Book {
     public void setYearOfPublication(int yearOfPublication) {//создание сеттера
 
         this.yearOfPublication = yearOfPublication;
+    }
+    @Override
+    public String toString(){
+        return "Название книги - " + this.getName() + ". " +
+                "Автор книги - " + this.author + ". " +
+                "Год публикации книги - " + this.yearOfPublication + ". ";
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book book = (Book) other;
+        return name.equals(book.name);
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(name);
     }
 }
